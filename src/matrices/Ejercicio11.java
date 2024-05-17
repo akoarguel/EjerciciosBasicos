56d0deb727a33398bca5165ef2aa1ee5bef00884
akoarguel <manel.arguelles@gmail.com>
2024-05-17 10:23:25 +0200

    17/05 casa

---
+++ src/matrices/Ejercicio11.java
@@ -0,0 +1,20 @@
+/* Queremos realizar una encuesta a 10 personas, en esta encuesta indicaremos el sexo 
+(1=masculino, 2=femenino), si trabaja (1=si trabaja, 2= no trabaja) y su sueldo (si tiene un trabajo, sino sera un cero) estará entre 600 y 2000 (valor entero). 
+Los valores pueden ser generados aleatoriamente. Calcula y muestra lo siguiente:
+
+Porcentaje de hombres (tengan o no trabajo).
+Porcentaje de mujeres (tengan o no trabajo).
+Porcentaje de hombres que trabajan.
+Porcentaje de mujeres que trabajan.
+El sueldo promedio de las hombres que trabajan.
+EL sueldo promedio de las mujeres que trabajan.
+Usa todos los métodos que veas necesarios, piensa que es aquello que se repite o que puede ser mejor tenerlo por separado.
+*/
+
+package matrices;
+
+public class Ejercicio11 {
+    public static void main(String[] args) {
+        
+    }
+}
