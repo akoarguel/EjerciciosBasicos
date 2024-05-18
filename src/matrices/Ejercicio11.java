7a21b3a6133b3e0839faed48015fe394aa12e233
ako <manel.arguelles@gmail.com>
2024-05-18 23:27:47 +0200

    18/05 casa

--- src/matrices/Ejercicio11.java
+++ src/matrices/Ejercicio11.java
@@ -1,4 +1,5 @@
-/* Queremos realizar una encuesta a 10 personas, en esta encuesta indicaremos el sexo 
+/* 
+Queremos realizar una encuesta a 10 personas, en esta encuesta indicaremos el sexo 
 (1=masculino, 2=femenino), si trabaja (1=si trabaja, 2= no trabaja) y su sueldo (si tiene un trabajo, sino sera un cero) estará entre 600 y 2000 (valor entero). 
 Los valores pueden ser generados aleatoriamente. Calcula y muestra lo siguiente:
 
@@ -15,6 +16,69 @@ package matrices;
 
 public class Ejercicio11 {
     public static void main(String[] args) {
+        System.out.println();
+        int encuestados = 10;
+        int[][] lista = new int[3][encuestados];
+
+        ingresarSexo(lista);
+        ingresarTrabajo(lista);
+        ingresarSalario(lista);
+        imprimirArray(lista);
         
+        System.out.println("\nHay un " + procentajeSexo(lista, 1) + "% de hombres. ");
+        System.out.println("Hay un " + procentajeSexo(lista, 2) + "% de mujeres. ");
+
+        System.out.println("\n");
+    }
+
+    
+
+    private static void imprimirArray(int[][] lista) {
+        for (int i = 0; i < lista.length; i++) {
+            for (int j = 0; j < lista[i].length; j++) {
+                System.out.print(lista[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    private static double procentajeSexo(int[][] lista, int i) {
+        int contador = 0;
+        for (int j = 0; j < lista[0].length; j++) {
+            if (lista[0][j] == i) {
+                contador+=1;
+            }
+        }
+        double porcentaje = (contador*100)/lista[0].length;
+        return porcentaje;
+    }
+
+
+
+    private static void ingresarSexo(int[][] lista) {
+        for (int j = 0; j < lista[0].length; j++) {
+            int num = (int) Math.floor(Math.random() * 2 + 1);
+            lista[0][j] = num;
+        }
+    }
+
+    private static void ingresarTrabajo(int[][] lista) {
+        for (int j = 0; j < lista[1].length; j++) {
+            int num = (int) Math.floor(Math.random() * 2 + 1);
+            lista[1][j] = num;
+        }
+    }
+
+    private static void ingresarSalario(int[][] lista) {
+        for (int i = 0; i < lista[1].length; i++) {
+            for (int j = 0; j < lista[2].length; j++) {
+                if (lista[1][j] == 2) {
+                    lista[2][j] = 0;
+                } else {
+                    int num = (int) Math.floor(Math.random() * 600 + 2000);
+                    lista[2][j] = num;
+                }
+            }
+        }
     }
 }
